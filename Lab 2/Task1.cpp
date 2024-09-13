dc5ddc122f3c4d1895b0959e0c3d5b64b08b38c9
“Farhan41229” <farhankhan@iut-dhaka.edu>
2024-09-13 08:36:12 +0600

    Made some changes to Task1 for better readabiligty

--- Lab 2/Task1.cpp
+++ Lab 2/Task1.cpp
@@ -2,16 +2,16 @@
 using namespace std;
 
 int main (){
-    int a,b,c,d;
+    int number1,number2,number3,number4;
     char dummychar;
     cout<<"Enter first fraction: "<<'\n';
-    cin >> a >> dummychar >> b;
+    cin >> number1 >> dummychar >> number2;
 
     cout<<"Enter second fraction: "<<'\n';
-    cin >> c >> dummychar >> d;
+    cin >> number3 >> dummychar >> number4;
 
-    int numerator = (a*d) + (b*c);
-    int denominator = (b*d);
+    int numerator = (number1*number4) + (number2*number3);
+    int denominator = (number2*number4);
 
     cout << "Sum = " << numerator << "/" << denominator << endl;
 
